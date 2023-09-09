393287ce1c0d143a882e500f4ff58168e1f2c6f1
Ahmad Nadil <88835532+IceTeaXXD@users.noreply.github.com>
2023-09-09 13:34:27 +0700

    fix: invalid bot move

--- src/Bot.java
+++ src/Bot.java
@@ -1,6 +1,31 @@
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javafx.scene.Cursor;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.RowConstraints;
+
+import java.io.IOException;
+
 public class Bot {
-    public int[] move() {
+    public int[] move(Button[][] buttons) {
         // create random move
-        return new int[]{(int) (Math.random()*8), (int) (Math.random()*8)};
+        int[] move = new int[2];
+        move[0] = (int) (Math.random() * 8);
+        move[1] = (int) (Math.random() * 8);
+        // check if move is valid
+        while (!buttons[move[0]][move[1]].getText().equals("")) {
+            move[0] = (int) (Math.random() * 8);
+            move[1] = (int) (Math.random() * 8);
+        }
+        return move;
     }
 }
