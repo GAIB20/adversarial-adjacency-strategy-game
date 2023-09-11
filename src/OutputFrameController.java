e98d0ff39fa4cdc26ef60f7c077bfe544ef9b5f0
Maria Khelli <13520115@std.stei.itb.ac.id>
2023-09-11 08:39:41 +0700

    [Fix] Game adjacent mechanism

--- src/OutputFrameController.java
+++ src/OutputFrameController.java
@@ -258,23 +258,29 @@ public class OutputFrameController {
         // Search for adjacency for X's and O's or vice versa, and replace them.
         // Update scores for X's and O's accordingly.
         for (int x = startRow; x <= endRow; x++) {
-            for (int y = startColumn; y <= endColumn; y++) {
-                if (this.playerXTurn) {
-                    if (this.buttons[x][y].getText().equals("O")) {
-                        this.buttons[x][y].setText("X");
-                        this.playerXScore++;
-                        this.playerOScore--;
-                    }
-                } else if (this.buttons[x][y].getText().equals("X")) {
-                    this.buttons[x][y].setText("O");
-                    this.playerOScore++;
-                    this.playerXScore--;
-                }
-            }
+            this.setPlayerScore(x, j);
+        }
+
+        for (int y = startColumn; y <= endColumn; y++) {
+            this.setPlayerScore(i, y);
         }
 
         this.playerXScoreLabel.setText(String.valueOf(this.playerXScore));
         this.playerOScoreLabel.setText(String.valueOf(this.playerOScore));
+    }
+
+    private void setPlayerScore(int i, int j){
+        if (this.playerXTurn) {
+            if (this.buttons[i][j].getText().equals("O")) {
+                this.buttons[i][j].setText("X");
+                this.playerXScore++;
+                this.playerOScore--;
+            }
+        } else if (this.buttons[i][j].getText().equals("X")) {
+            this.buttons[i][j].setText("O");
+            this.playerOScore++;
+            this.playerXScore--;
+        }
     }
 
 
